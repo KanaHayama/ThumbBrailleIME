4ab267592b4607b3e7944b9bec1fc46902ad45da
EstherTMY <mtang724@gmail.com>
2019-04-07 00:10:03 -0700

    add tts and listview(menu)

--- app/src/main/java/com/tbraille/android/MainActivity.java
+++ app/src/main/java/com/tbraille/android/MainActivity.java
@@ -1,13 +1,51 @@
 package com.tbraille.android;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView listview;
+    // 模拟数据
+    private List<String> dataList = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataList = new ArrayList<String>();
+        dataList.add("Go to Speech");
+        //dataList.add("Setting sample")
+        // Setting adapter
+        ListAdapter adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, dataList);
+        listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+        // bind item click event
+        listview.setOnItemClickListener(this);
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // position is the position of which data you are clicking
+        if(position == 0) {
+            Intent intent = new Intent(MainActivity.this, SpeechActivity.class);
+            startActivity(intent);
+        }
+//        Toast.makeText(MainActivity.this, "Click on" + position + "",0).show();
     }
 }
---
+++ app/src/main/java/com/tbraille/android/SpeechActivity.java
@@ -0,0 +1,71 @@
+package com.tbraille.android;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class SpeechActivity extends AppCompatActivity {
+
+    private EditText input;
+    private Button speech,record;
+
+    private TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == textToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.ENGLISH);
+                    if (result != TextToSpeech.LANG_COUNTRY_AVAILABLE
+                            && result != TextToSpeech.LANG_AVAILABLE){
+                        Toast.makeText(SpeechActivity.this, "TTS is not support this language！",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        input = (EditText) findViewById(R.id.input_text);
+        speech = (Button) findViewById(R.id.speech);
+//        record = (Button) findViewById(R.id.record);
+
+        speech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(input.getText().toString(),
+                        TextToSpeech.QUEUE_ADD, null);
+            }
+        });
+
+//        record.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String inputText = input.getText().toString();
+//                HashMap<String, String> myHashRender = new HashMap<>();
+//                myHashRender.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, inputText);
+//                textToSpeech.synthesizeToFile(inputText, myHashRender,
+//                        "/mnt/sdcard/my_recorder_audios/sound.wav");
+//                Toast.makeText(SpeechActivity.this, "声音记录成功。", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null)
+            textToSpeech.shutdown();
+        super.onDestroy();
+    }
+}
